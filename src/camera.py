a10c96a1d3baa2d4cb67e9d56b44cd4929fd91a7
Jan Stachurski <jan.stachurski@outlook.com>
2024-04-08 21:26:48 +0200

    everything works!

--- src/camera.py
+++ src/camera.py
@@ -15,28 +15,21 @@ class Camera:
 
     # find intersection with viewing plane in camera space
     def plane_intersection(self, point):
-        # -t = d / (d + z)
-        # nt = self._depth / (self._depth + point[2])
-        # p' = (0 - tx, 0 - ty, -d - tz - td)
-        # return np.array([nt * point[0], nt * point[1], 0], dtype=float)
         t = self._depth / point[2]
         return t * point
 
     def move(self, translation):
         self._view_center += self.x_axis_in_scene() * translation[0] + self.y_axis_in_scene() * translation[1] + self.z_axis_in_scene() * translation[2]
-
+    
     def x_rotation(self, angle):
-        #self.rotate(np.array([1, 0, 0], dtype=float), angle)
-        self._rotation *= rotations.axis_angle_to_quat(self.x_axis_in_scene(), angle)
+        self._rotation *= rotations.axis_angle_to_quat(np.array([1, 0, 0], dtype=float), angle)
 
     def y_rotation(self, angle):
-        #self.rotate(np.array([0, 1, 0], dtype=float), angle)
-        self._rotation *= rotations.axis_angle_to_quat(self.y_axis_in_scene(), angle)
+        self._rotation *= rotations.axis_angle_to_quat(np.array([0, 1, 0], dtype=float), angle)
 
     def z_rotation(self, angle):
-        #self.rotate(np.array([0, 0, 1], dtype=float), angle)
-        self._rotation *= rotations.axis_angle_to_quat(self.z_axis_in_scene(), angle)
-    
+        self._rotation *= rotations.axis_angle_to_quat(np.array([0, 0, 1], dtype=float), angle)
+
     def x_axis_in_scene(self):
         return rotations.quat_rotate(np.array([1, 0, 0], dtype=float), self._rotation)
     
@@ -46,19 +39,12 @@ class Camera:
     def z_axis_in_scene(self):
         return rotations.quat_rotate(np.array([0, 0, 1], dtype=float), self._rotation)
 
-    # def rotate(self, axis, angle):
-    #     world_axis = rotations.quat_rotate(axis, self._rotation.inversed())
-    #     print(world_axis)
-    #     self._rotation *= rotations.axis_angle_to_quat(world_axis, angle)
-    #     #self._rotation *= rotations.axis_angle_to_quat(axis, angle)
-
     def scene_to_camera_space(self, point):
         diff = point - self._view_center
         xp = np.dot(diff, self.x_axis_in_scene())
         yp = np.dot(diff, self.y_axis_in_scene())
         zp = np.dot(diff, self.z_axis_in_scene())
         return np.array([xp, yp, zp], dtype=float)
-        #return rotations.quat_rotate(point - self._view_center, self._rotation) - self._view_center
     
     def edge_to_camera_space(self, edge: Edge):
         a = self.scene_to_camera_space(edge.a)
@@ -77,10 +63,6 @@ class Camera:
     
     def shot_scene(self, scene: list[Edge]) -> list[Edge]:
         res = []
-        # scene = []
-        # scene.append(Edge(np.array([0, 0, 0], dtype=float), self.x_axis_in_scene()))
-        # scene.append(Edge(np.array([0, 0, 0], dtype=float), self.y_axis_in_scene()))
-        # scene.append(Edge(np.array([0, 0, 0], dtype=float), self.z_axis_in_scene()))
         for edge in scene:
             edge_in_camera = self.edge_to_camera_space(edge)
             if edge_in_camera is not None:
@@ -91,6 +73,3 @@ class Camera:
         x = point[0] * display_half_width / self._half_width + display_half_width
         y = point[1] * display_half_height / self._half_height + display_half_height
         return x, y
-
-    # def __repr__(self):
-    #     return f'Camera\n    view_center: {self._view_center}\n    focal_point: {self._focal_point}\n    rotation: \n{self._rotation}\n'
